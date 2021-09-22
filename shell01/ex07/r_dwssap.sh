780bee39f37874e20f9bf04deb62a946bce282c7
Abdessalam Bahafid <zomiix246@gmail.com>
2021-09-22 15:53:36 +0100

    fix ex07

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat /etc/passwd | grep '^[^#]' | awk "!(NR % 2)" | cut -d: -f1 | rev  | sort -r | awk -v FT_LINE1=$FT_LINE1 -v FT_LINE2=$FT_LINE2 'NR >= FT_LINE1 && NR <= FT_LINE2 { print $0 }' | tr '\n' ',' | sed 's/,$/./'
+cat /etc/passwd | grep '^[^#]' | awk "!(NR % 2)" | cut -d: -f1 | rev  | sort -r | awk -v FT_LINE1=$FT_LINE1 -v FT_LINE2=$FT_LINE2 'NR >= FT_LINE1 && NR <= FT_LINE2 { print $0 }' | tr -s '\n' ',' | sed 's/,/, /g'  | sed 's/, $/./'
