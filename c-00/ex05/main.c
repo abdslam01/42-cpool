05eb9c47d100544c53e5308434ade76796f76351
Abdessalam Bahafid <zomiix246@gmail.com>
2021-09-23 10:30:44 +0100

    add c00 ex05

--- c-00/ex05/main.c
+++
@@ -1,6 +0,0 @@
-void ft_print_comb();
-int	main(void)
-{
-	ft_print_comb();
-	return (0);
-}
--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -6,39 +6,44 @@
 /*   By: abahafid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 17:53:29 by abahafid          #+#    #+#             */
-/*   Updated: 2021/09/22 19:24:55 by abahafid         ###   ########.fr       */
+/*   Updated: 2021/09/23 09:48:10 by abahafid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_write_output(char i, char j, char k, short is_last_time)
+{
+	write(1, &i, 1);
+	write(1, &j, 1);
+	write(1, &k, 1);
+	if (!is_last_time)
+		write (1, ", ", 2);
+}
+
 void	ft_print_comb(void)
 {
-	short	i;
-	short	j;
-	short	k;
-	short	is_first_print;
+	char	i;
+	char	j;
+	char	k;
+	short	is_last_time;
 
-	is_first_print = 0;
-	i = 0;
-	while (i < 10)
+	is_last_time = 0;
+	i = '0';
+	while (i <= '7')
 	{
 		j = i + 1;
-		while (j < 10)
+		while (j <= '8')
 		{
 			k = j + 1;
-			while (k < 10)
+			while (k <= '9')
 			{
-				write(1, (i*100 + j*10 + k) + "", 3);
-				if (is_first_print == 0)
-				{
-					is_first_print = 1;
-					continue;
-				}
-				write(1, ", ", 2);
+				if (i == '7' && j == '8' && k == '9')
+					is_last_time = 1;
+				ft_write_output(i, j, k, is_last_time);
 				k++;
 			}
-		j++;	
+			j++;
 		}
 		i++;
 	}
