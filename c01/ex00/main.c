8ea394d8f7e1bc7f8529ad9473e6752c3bd2a1c3
Abdessalam Bahafid <zomiix246@gmail.com>
2021-09-25 18:30:06 +0100

    starting c01

---
+++ c01/ex00/main.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		if (nb == -2147483648)
+		{
+			write(1, "-2147483648", 11);
+			return ;
+		}
+		nb *= -1;
+		ft_putchar('-');
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + '0');
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+}
+
+void    ft_ft(int *nbr);
+
+int main(void)
+{
+    int a;
+    ft_ft(&a);
+    ft_putnbr(a);
+    return (0);
+}
---
+++ c01/ex01/main.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		if (nb == -2147483648)
+		{
+			write(1, "-2147483648", 11);
+			return ;
+		}
+		nb *= -1;
+		ft_putchar('-');
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + '0');
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+}
+
+void	ft_ultimate_ft(int *********nbr);
+
+int main(void)
+{
+    //int	*n1;
+    //int	**n2;
+    //int	***n3;
+    //int	****n4;
+    //int	*****n5;
+    //int	******n6;
+    //int	*******n7;
+    //int	********n8;
+    int	********n9;
+
+	//n2 = &n1;
+	//n3 = &n2;
+	//n4 = &n3;
+	//n5 = &n4;
+	//n6 = &n5;
+	//n7 = &n6;
+	//n8 = &n7;
+	//n9 = &n8;
+    ft_ultimate_ft(&n9);
+    ft_putnbr(*********n9);
+    return (0);
+}
