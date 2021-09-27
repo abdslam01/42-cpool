b830f5e11cf027eb7d3eb8df059c857a308ed463
Abdessalam Bahafid <zomiix246@gmail.com>
2021-09-27 18:20:17 +0100

    fix c02 ex12

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -6,16 +6,11 @@
 /*   By: abahafid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 19:13:04 by abahafid          #+#    #+#             */
-/*   Updated: 2021/09/27 15:49:11 by abahafid         ###   ########.fr       */
+/*   Updated: 2021/09/27 18:18:02 by abahafid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-int	ft_is_printable(char c)
-{
-	return (c >= ' ' && c <= '~');
-}
 
 void	ft_print_address(unsigned long add)
 {
@@ -27,24 +22,24 @@ void	ft_print_address(unsigned long add)
 	{
 		i -= 4;
 		tmp = add >> i & 0xf;
-		if(tmp > 9)
+		if (tmp > 9)
 			tmp = tmp + 'a' - 10;
 		else
 			tmp = tmp + '0';
-		write(1, &tmp, 1);	
+		write(1, &tmp, 1);
 	}
 }
 
 void	ft_print_data_hex(char *add)
 {
-	int left;
-	int right;
+	int	left;
+	int	right;
 	int	i;
 
 	i = 0;
-	while (add[i])
+	while (add[i] && i < 16)
 	{
-		if (i % 4 == 0)
+		if (i % 2 == 0)
 			write(1, " ", 1);
 		left = add[i] >> 4;
 		right = add[i++] & 0xf;
@@ -58,38 +53,52 @@ void	ft_print_data_hex(char *add)
 			right += '0';
 		write(1, &left, 1);
 		write(1, &right, 1);
-		i++;
 	}
+	if (!*add)
+		write(1, "00", 2);
 }
 
 void	ft_print_data_ascii(char *add)
 {
 	int	i;
 
-	i = -1;
-	while (add[++i])
+	i = 0;
+	while (add[i] && i < 16)
 	{
-		if(ft_is_printable(add[i]))
+		if (add[i] >= ' ' && add[i] <= '~')
 			write(1, add + i, 1);
 		else
 			write(1, ".", 1);
+		i++;
 	}
+	if (!*add)
+		write(1, ".", 1);
 }
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
-	size = 10;
-	ft_print_address((unsigned long) addr);
-	write(1, ":", 1);
-	ft_print_data_hex((char *) addr);
-	write(1, " ", 1);
-	ft_print_data_ascii((char *) addr);
-	return (addr);
+	unsigned int	i;
+	void			*return_addr;
+
+	return_addr = addr;
+	i = 0;
+	while (i < size)
+	{
+		ft_print_address((unsigned long) addr);
+		write(1, ":", 1);
+		ft_print_data_hex((char *) addr);
+		write(1, " ", 1);
+		ft_print_data_ascii((char *) addr);
+		write(1, "\n", 1);
+		i += 16;
+		addr += 16;
+	}
+	return (return_addr);
 }
 
-int	main()
+int	main(void)
 {
-	char *s = "Bonjour les a\nin";
-	ft_print_memory(s, 16);
+	char *s = "";//Bonjour les aminches\n\n\nc est fo u";
+	ft_print_memory(s, 1);
 	
 }
