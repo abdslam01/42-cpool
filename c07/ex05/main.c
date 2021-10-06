4e4578d5feb5ab12e6910e73f87af3cb7c530902
Abdessalam Bahafid <zomiix246@gmail.com>
2021-10-06 17:19:22 +0100

    update test c07/ex05/main.c

--- c07/ex05/main.c
+++ c07/ex05/main.c
@@ -5,13 +5,14 @@ char	**ft_split(char *str, char *charset);
 
 int main(void)
 {
-	char **s = ft_split(",hello,world,,one,", ",");
+	//char **s = ft_split(",hello,world,,one,", ",");
+	char **s = ft_split("             ", "  ");
 	int i=0;
-	while (s[i][0])
+	while (s[i])
 	{
 		printf("%s\n", s[i]);
 		free(s[i++]);
 	}
-	free(s[i]); // free the terminating pointer
 	free(s);
+	printf("executed correctly...");
 }
