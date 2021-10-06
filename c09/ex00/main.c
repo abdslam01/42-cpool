239d7242492ee4e527c9103a17eeb6b6e4a79860
Abdessalam Bahafid <zomiix246@gmail.com>
2021-10-06 20:03:06 +0100

    start c09

---
+++ c09/ex00/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
+
+int main(void)
+{
+	
+}
