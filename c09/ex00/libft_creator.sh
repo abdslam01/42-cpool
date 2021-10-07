01acd8ffa5d252aa41b3e00d322e6ff86cec510a
Abdessalam Bahafid <zomiix246@gmail.com>
2021-10-07 09:45:42 +0100

    be aware of wildcards

--- c09/ex00/libft_creator.sh
+++ c09/ex00/libft_creator.sh
@@ -6,24 +6,21 @@
 #    By: abahafid <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/07 08:31:45 by abahafid          #+#    #+#              #
-#    Updated: 2021/10/07 09:27:43 by abahafid         ###   ########.fr        #
+#    Updated: 2021/10/07 09:38:54 by abahafid         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
-files=`ls ft*.c`
+files="ft_putchar.c ft_strcmp.c ft_swap.c ft_putstr.c ft_strlen.c"
 for file in $files; do
 	curr_file=`echo $file | cut -d. -f1`
 	gcc -c $curr_file.c -o $curr_file.o
-	echo creating $curr_file.o
 done
 
 ar rc libft.a ft*.o
-echo -----creating library
 
 for file in $files; do
 	curr_file=`echo $file | cut -d. -f1`
 	rm -rf $curr_file.o
-	echo deleting $curr_file.o
 done
