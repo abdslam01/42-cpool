73bdf4aa722da315091dffef00dfa9d58cf66feb
Abdessalam Bahafid <zomiix246@gmail.com>
2021-10-09 17:50:46 +0100

    update c10/ex01

--- c10/ex01/srcs/ft_write_files_to_stdout.c
+++ c10/ex01/srcs/ft_write_files_to_stdout.c
@@ -6,7 +6,7 @@
 /*   By: abahafid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 11:01:12 by abahafid          #+#    #+#             */
-/*   Updated: 2021/10/09 15:58:56 by abahafid         ###   ########.fr       */
+/*   Updated: 2021/10/09 17:46:33 by abahafid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,35 +21,45 @@ void	ft_put_error(char *file, char *err_str)
 	write(1, "\n", 1);
 }
 
-void	ft_write_files_to_stdout(int count, char **files)
-{
-	int		i;
+void	ft_write_file(char *file, int *state)
+{	
 	int		fd;
 	int		c;
 	char	*err_str;
 
-	i = -1;
-	while (++i < count)
+	if (file[0] == '-' && !file[1])
 	{
-		if (files[i][0] == '-' && !files[i][1])
+		if (*state)
 		{
 			ft_write_stdin_to_stdout();
-			continue ;
+			*state = 0;
 		}
-		fd = open(files[i], O_RDWR);
-		err_str = strerror(errno);
-		if (errno)
-		{
-			ft_put_error(files[i], err_str);
-			errno = 0;
-		}
-		else if (fd < 0)
-			ft_putstr("Cannot read file.\n");
-		else
-		{
-			while (read(fd, &c, 1))
-				write(1, &c, 1);
-			close(fd);
-		}
+		return ;
 	}
+	fd = open(file, O_RDWR);
+	err_str = strerror(errno);
+	if (errno)
+	{
+		ft_put_error(file, err_str);
+		errno = 0;
+	}
+	else if (fd < 0)
+		ft_putstr("Cannot read file.\n");
+	else
+	{
+		while (read(fd, &c, 1))
+			write(1, &c, 1);
+		close(fd);
+	}
+}
+
+void	ft_write_files_to_stdout(int count, char **files)
+{
+	int	i;
+	int	state_dash_only_once;
+
+	state_dash_only_once = 1;
+	i = -1;
+	while (++i < count)
+		ft_write_file(files[i], &state_dash_only_once);
 }
