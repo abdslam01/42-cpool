697d2782006b411dd31cbcae983fa26fea9d0d89
Abdessalam Bahafid <zomiix246@gmail.com>
2021-10-14 18:50:36 +0100

    fix: ex05/ft_strlcat.c

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,13 +6,13 @@
 /*   By: abahafid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 14:22:37 by abahafid          #+#    #+#             */
-/*   Updated: 2021/10/02 08:28:13 by abahafid         ###   ########.fr       */
+/*   Updated: 2021/10/14 18:46:47 by abahafid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlen(char *s)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (s[i])
@@ -24,19 +24,19 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 	unsigned int	j;
-	unsigned int	len_s;
-	unsigned int	len_d;
+	unsigned int	l_src;
+	unsigned int	l_dest;
 
-	len_s = ft_strlen(src);
-	len_d = ft_strlen(dest);
+	l_src = ft_strlen(src);
+	l_dest = ft_strlen(dest);
 	if (!size)
-		return (len_s);
-	else if (len_d <= size)
-		return (len_s + len_d);
-	i = len_d;
+		return (l_src);
+	i = l_dest;
 	j = 0;
-	while (src[j] && j < size - len_d - 1)
+	if (size <= i)
+		return (l_src + size);
+	while (src[j] && i < size - 1)
 		dest[i++] = src[j++];
 	dest[i] = 0;
-	return (len_s + size);
+	return (l_dest + l_src);
 }
