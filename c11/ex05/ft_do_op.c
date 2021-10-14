14baae54917025573f7c616c375199aad02cb7f3
Abdessalam Bahafid <zomiix246@gmail.com>
2021-10-14 12:43:05 +0100

    refix c11/ex05

--- c11/ex05/ft_do_op.c
+++ c11/ex05/ft_do_op.c
@@ -6,7 +6,7 @@
 /*   By: abahafid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 20:08:12 by abahafid          #+#    #+#             */
-/*   Updated: 2021/10/13 19:05:23 by abahafid         ###   ########.fr       */
+/*   Updated: 2021/10/14 12:14:57 by abahafid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int argc, char **argv)
 	oper_pos = find_op(argv[2][0], "+-*/%");
 	n1 = ft_atoi(argv[1]);
 	n2 = ft_atoi(argv[3]);
-	if (oper_pos >= 0 && oper_pos <= 4)
+	if (oper_pos >= 0 && oper_pos <= 4 && argv[2][1] == 0)
 	{
 		calculate(n1, n2, (*opers[oper_pos]));
 		ft_putstr("\n");
